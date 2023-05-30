3db7453cef254c04aec63c3082d7150a6ff29e2a
Saeta07 <saeta.vj@gmail.com>
2023-05-30 18:18:28 -0300

    Initial commit

---
+++ src/main/java/com/proyectosena/shokuapp/ShokuappApplication.java
@@ -0,0 +1,13 @@
+package com.proyectosena.shokuapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShokuappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShokuappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proyectosena/shokuapp/ShokuappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyectosena.shokuapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShokuappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
